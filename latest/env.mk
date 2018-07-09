cc2ff91f9b67042a2f962e6cf072a0db2bac6ca9
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2018-07-09 13:55:02 +0200

    first commit

---
+++ latest/env.mk
@@ -0,0 +1,9 @@
+IMAGE_NAME =            pritunl
+IMAGE_VERSION =         latest
+VERSION_ALIASES =       1.29.1758.63 1.19 1
+IMAGE_TITLE =           pritunl
+IMAGE_DESCRIPTION =     Enterprise VPN server
+IMAGE_SOURCE_URL =      https://github.com/scaleway-community/scaleway-pritunl
+IMAGE_VENDOR_URL =      https://github.com/pritunl/pritunl
+IMAGE_BOOTSCRIPT =      mainline 4.4
+IMAGE_BASE_FLAVORS =    common docker-based systemd feature-motd
